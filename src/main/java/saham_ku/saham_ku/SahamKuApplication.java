965d521177076c9ed2175c91a1c76b4f892975af
hendy <henza224@gmail.com>
2024-07-05 20:00:41 +0700

    first commit

---
+++ src/main/java/saham_ku/saham_ku/SahamKuApplication.java
@@ -0,0 +1,13 @@
+package saham_ku.saham_ku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SahamKuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SahamKuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/saham_ku/saham_ku/SahamKuApplicationTests.java
@@ -0,0 +1,13 @@
+package saham_ku.saham_ku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SahamKuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
